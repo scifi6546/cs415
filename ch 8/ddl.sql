458220a0823a1f35c739e4314e9569f437d53bf3
Nicholas Alexeev <nickalexeev@protonmail.com>
2021-04-20 09:07:14 -0800

    first commit

---
+++ ch 8/ddl.sql
@@ -0,0 +1,73 @@
+CREATE TABLE InspectionType(
+    InspectionType INT NOT NULL,
+    InspectionDescription VARCHAR(37) NOT NULL,
+    PRIMARY KEY(InspectionType)
+);
+CREATE TABLE Result(
+    ResultType INT NOT NULL,
+    ResultDescription VARCHAR(37) NOT NULL,
+    PRIMARY KEY(ResultType)
+);
+CREATE TABLE Action(
+    ActionType INT NOT NULL,
+    ActionDescription VARCHAR(34) NOT NULL,
+    PRIMARY KEY(ActionType)
+);
+CREATE TABLE Violation(
+    ViolationType VARCHAR(6) NOT NULL,
+    ViolationDescription VARCHAR(99),
+    PRIMARY KEY(ViolationType)
+);
+
+CREATE TABLE Program(
+    ProgramCategory INT NOT NULL,
+    ProgramCategoryDescription VARCHAR(40) NOT NULL,
+    PRIMARY KEY(ProgramCategory)
+);
+
+CREATE TABLE SiteAddress(
+    AddressCode INT NOT NULL AUTO_INCREMENT,
+    City CHAR(11) NOT NULL,
+    State CHAR(2) NOT NULL,
+    Zip VARCHAR(10) NOT NULL,
+    StreetNumber VARCHAR(7) NOT NULL,
+    StreetType VARCHAR(4) DEFAULT NULL,
+    StreetName VARCHAR(24) NOT NULL,
+    PostalDirection VARCHAR(2) DEFAULT NULL,
+    PRIMARY KEY(AddressCode)
+);
+CREATE TABLE Owner(
+    OwnerKey INT NOT NULL,
+    OwnerName VARCHAR(55) NOT NULL,
+    PRIMARY KEY(OwnerKey)
+);
+
+CREATE TABLE Facility(
+    FacilityKey INT NOT NULL,
+    NatureofBusiness VARCHAR(25),
+    FacilityName VARCHAR(100) NOT NULL,
+    AddressCode INT,
+    Phone VARCHAR(20) DEFAULT NULL,
+    OwnerKey INT,
+    PRIMARY KEY(FacilityKey),
+    Foreign Key(AddressCode) REFERENCES SiteAddress(AddressCode),
+    Foreign Key(OwnerKey) REFERENCES Owner(OwnerKey)
+);
+CREATE TABLE Inspection(
+    InspectionId int Not NULL AUTO_INCREMENT,
+    InspectionDate date DEFAULT NULL,
+    InspectionMemo varchar(632),
+    InspectionType int,
+    ActionType int NOT NULL,
+    ResultType int NOT NULL,
+    ViolationType VARCHAR(6) NOT NULL,
+    ProgramType int NOT NULL,
+    FacilityKey int NOT NULL,
+    PRIMARY KEY(InspectionId),
+    Foreign key (InspectionType) REFERENCES InspectionType(InspectionType),
+    Foreign Key (ActionType) REFERENCES Action(ActionType),
+    Foreign Key (ResultType) REFERENCES Result(ResultType),
+    Foreign Key (ViolationType) REFERENCES Violation(ViolationType),
+    Foreign Key (ProgramType) REFERENCES Program(ProgramCategory),
+    Foreign Key(FacilityKey) REFERENCES Facility(FacilityKey)
+);
